9badf1f2fbab264a4c652841e428a89367d1253d
Antonin Kmoch <antonin.kmoch@plus4u.net>
2017-12-05 22:19:12 +0100

    first draft

--- day2.py
+++ day2.py
@@ -5,10 +5,9 @@ f.close()
 def findIt(arr):
     for a in arr:
         for b in arr:
-            if (a != b and (a % b == 0 or b % a == 0)):
-                print(a,b,a/b)
-                return a/b
+            if (a != b and max(a,b) % min(a,b) == 0):
+                return int(max(a,b)/min(a,b))
     return 0
 f = open('day2.txt')
-print([findIt(splittedRow) for splittedRow in [sorted(map(int, row.strip().split('\t'))) for row in f]])
+print(sum([findIt(splittedRow) for splittedRow in [sorted(map(int, row.strip().split('\t'))) for row in f]]))
 f.close()
